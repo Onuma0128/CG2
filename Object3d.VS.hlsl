5267605df5a009021c390a2516187c5c8cfc3f47
Onuma0128 <147121758+Onuma0128@users.noreply.github.com>
2024-04-23 19:18:28 +0900

    CompileShader関数を作成

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -1,7 +1,7 @@
-float4 main(float4 pos : POSITION) : SV_POSITION
-{
-    return pos;
-}
+//float4 main(float4 pos : POSITION) : SV_POSITION
+//{
+//    return pos;
+//}
 struct VertexShaderOutput
 {
     float32_t4 position : SV_POSITION;
