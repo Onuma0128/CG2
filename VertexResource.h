680c293401a11baebb8ccb478ef40b1feea2496a
Onuma0128 <147121758+Onuma0128@users.noreply.github.com>
2024-07-03 12:10:08 +0900

    修正

--- VertexResource.h
+++ VertexResource.h
@@ -18,60 +18,60 @@ public:
 	void Initialize(ComPtr<ID3D12Device> device);
 
 	void Update();
-	ModelData& GetModelData() { return modelData; }
-	D3D12_VERTEX_BUFFER_VIEW& GetVertexBufferView() { return vertexBufferView; }
-	D3D12_VERTEX_BUFFER_VIEW& GetVertexBufferViewSprite() { return vertexBufferViewSprite; }
-	D3D12_INDEX_BUFFER_VIEW& GetIndexBufferViewSprite() { return indexBufferViewSprite; }
-	ComPtr<ID3D12Resource> GetDirectionalLightResource() { return directionalLightResource; }
-	ComPtr<ID3D12Resource> GetMaterialResource() { return materialResource; }
-	ComPtr<ID3D12Resource> GetMaterialResourceSprite() { return materialResourceSprite; }
-	ComPtr<ID3D12Resource> GetwvpResource() { return wvpResource; }
-	ComPtr<ID3D12Resource> GetTransformationMatrixResourceSprite() { return transformationMatrixResourceSprite; }
+	ModelData& GetModelData() { return modelData_; }
+	D3D12_VERTEX_BUFFER_VIEW& GetVertexBufferView() { return vertexBufferView_; }
+	D3D12_VERTEX_BUFFER_VIEW& GetVertexBufferViewSprite() { return vertexBufferViewSprite_; }
+	D3D12_INDEX_BUFFER_VIEW& GetIndexBufferViewSprite() { return indexBufferViewSprite_; }
+	ComPtr<ID3D12Resource> GetDirectionalLightResource() { return directionalLightResource_; }
+	ComPtr<ID3D12Resource> GetMaterialResource() { return materialResource_; }
+	ComPtr<ID3D12Resource> GetMaterialResourceSprite() { return materialResourceSprite_; }
+	ComPtr<ID3D12Resource> GetwvpResource() { return wvpResource_; }
+	ComPtr<ID3D12Resource> GetTransformationMatrixResourceSprite() { return transformationMatrixResourceSprite_; }
 
 	void ImGui(bool& useMonsterBall);
 
 private:
 	//モデル読み込み
-	ModelData modelData;
+	ModelData modelData_;
 	//実際に頂点リソースを作る
-	ComPtr<ID3D12Resource> vertexResource = nullptr;
+	ComPtr<ID3D12Resource> vertexResource_ = nullptr;
 	//Sprite用の頂点リソースを作る
-	ComPtr<ID3D12Resource> vertexResourceSprite = nullptr;
-	ComPtr<ID3D12Resource> indexResourceSprite = nullptr;
+	ComPtr<ID3D12Resource> vertexResourceSprite_ = nullptr;
+	ComPtr<ID3D12Resource> indexResourceSprite_ = nullptr;
 	//平行光源用のリソースを作る
-	ComPtr<ID3D12Resource> directionalLightResource = nullptr;
+	ComPtr<ID3D12Resource> directionalLightResource_ = nullptr;
 	//VertexBufferViewを作成する
 	//頂点バッファビューを作成する
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite{};
-	D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite{};
-	D3D12_VERTEX_BUFFER_VIEW directionalLightBufferView{};
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferView_{};
+	D3D12_VERTEX_BUFFER_VIEW vertexBufferViewSprite_{};
+	D3D12_INDEX_BUFFER_VIEW indexBufferViewSprite_{};
+	D3D12_VERTEX_BUFFER_VIEW directionalLightBufferView_{};
 	//頂点リソースにデータを書き込む
-	VertexData* vertexData = nullptr;
-	VertexData* vertexDataSprite = nullptr;
-	uint32_t* indexDataSprite = nullptr;
-	DirectionalLight* directionalLightData = nullptr;
-	//分割数
-	uint32_t vertexCount = 16;
+	VertexData* vertexData_ = nullptr;
+	VertexData* vertexDataSprite_ = nullptr;
+	uint32_t* indexDataSprite_ = nullptr;
+	DirectionalLight* directionalLightData_ = nullptr;
+	//分割数(球体)
+	uint32_t vertexCount_ = 16;
 	//マテリアル用のリソースを作る
-	ComPtr<ID3D12Resource> materialResource = nullptr;
-	ComPtr<ID3D12Resource> materialResourceSprite = nullptr;
+	ComPtr<ID3D12Resource> materialResource_ = nullptr;
+	ComPtr<ID3D12Resource> materialResourceSprite_ = nullptr;
 	//マテリアルにデータを書き込む
-	Material* materialData = nullptr;
-	Material* materialDataSprite = nullptr;
+	Material* materialData_ = nullptr;
+	Material* materialDataSprite_ = nullptr;
 	//WVP用のリソースを作る
-	ComPtr<ID3D12Resource> wvpResource = nullptr;
+	ComPtr<ID3D12Resource> wvpResource_ = nullptr;
 	//Sprite用
-	ComPtr<ID3D12Resource> transformationMatrixResourceSprite = nullptr;
+	ComPtr<ID3D12Resource> transformationMatrixResourceSprite_ = nullptr;
 	//データを書き込む
-	TransformationMatrix* wvpData = nullptr;
-	TransformationMatrix* transformationMatrixDataSprite = nullptr;
+	TransformationMatrix* wvpData_ = nullptr;
+	TransformationMatrix* transformationMatrixDataSprite_ = nullptr;
 
 	//Transform変数を作る
-	Transform transform{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
+	Transform transform_{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
 	Transform transformSprite{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
 	//Camera変数を作る
-	Transform cameraTransform{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,-10.0f} };
+	Transform cameraTransform{ {1.0f,1.0f,1.0f},{0.25f,0.0f,0.0f},{0.0f,2.5f,-10.0f} };
 	//UVTransform変数
 	Transform uvTransformSprite{ {1.0f,1.0f,1.0f},{0.0f,0.0f,0.0f},{0.0f,0.0f,0.0f} };
 	Matrix4x4 uvTransformMatrix{};
